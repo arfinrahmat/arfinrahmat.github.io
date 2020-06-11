97bae61b1ce7533713d345ebec57d330aba15951
Arfin Rahmat <arfinrahmat13@gmail.com>
2020-06-11 16:45:25 +0700

    deploy

---
+++ precache-manifest.8221e71ef06e17a36c1994ef133becf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa5bf43636b81d298893",
+    "url": "/static/css/main.ac51d101.chunk.css"
+  },
+  {
+    "revision": "aa5bf43636b81d298893",
+    "url": "/static/js/main.aa5bf436.chunk.js"
+  },
+  {
+    "revision": "0b3217ece884079f3f11",
+    "url": "/static/js/1.0b3217ec.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "85be5a76f9901399c840555ca855a40d",
+    "url": "/index.html"
+  }
+];
